ac9c6e29c2e196ffbc718718fde8777ac7eb7539
cdnls <sarepna@gmail.com>
2025-10-24 11:17:01 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,34 @@
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   const targetUrl = req.query.url;
-  const response = await fetch(targetUrl);
-  const html = await response.text();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'text/html');
-  res.send(html);
-}
+
+  if (!targetUrl || !/^https?:\/\//.test(targetUrl)) {
+    return res.status(400).send('URL tidak valid atau tidak diberikan.');
+  }
+
+  try {
+    const response = await fetch(targetUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Referer': 'https://codepen.io/',
+        'Accept': 'text/html'
+      }
+    });
+
+    const html = await response.text();
+
+    const minified = html
+      .replace(/<!--(?!<!)[^\[>].*?-->/g, '')
+      .replace(/>\s+</g, '><')
+      .replace(/\n|\r|\t/g, '')
+      .replace(/\s{2,}/g, ' ')
+      .trim();
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'text/html');
+    res.send(minified);
+  } catch (err) {
+    res.status(500).send(`<pre>${err.toString()}</pre>`);
+  }
+};
